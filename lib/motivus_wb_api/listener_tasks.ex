a83629f90fbf9e901d3cee94eaa1701ffe8f6465
Felipe Saldias <felipesaldias@gmail.com>
2021-02-08 14:15:52 -0300

    FS/SR/NR: add flop to task listener

--- lib/motivus_wb_api/listener_tasks.ex
+++ lib/motivus_wb_api/listener_tasks.ex
@@ -23,7 +23,8 @@ defmodule MotivusWbApi.ListenerTasks do
         date_in: DateTime.truncate(DateTime.utc_now(), :second),
         attempts: 0,
         processing_base_time: data[:body]["processing_base_time"],
-        flops: data[:body]["flops"]
+        flops: data[:body]["flops"],
+        flop: data[:body]["flop"]
       }
       |> Repo.insert!()
 
