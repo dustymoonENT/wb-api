be761a35a5ed9a1dbecab3b65e26138fee557bf8
Fernando Mora <f.mora.g90@gmail.com>
2021-09-13 09:23:55 -0300

    CH|SE|FM Update response struct, event name

--- lib/motivus_wb_api/listener_completed.ex
+++ lib/motivus_wb_api/listener_completed.ex
@@ -37,17 +37,20 @@ defmodule MotivusWbApi.ListenerCompleted do
 
     MotivusWbApi.QueueProcessing.drop(MotivusWbApi.QueueProcessing, id, tid)
 
-    result = %{time: Enum.at(body,0), flops: Enum.at(body,1), rvs: Enum.at(body,2)}
+    result = body
+    # result = %{time: Enum.at(body,0), flops: Enum.at(body,1), rvs: Enum.at(body,2)}
     Repo.get_by(Task, id: task_id, user_id: user.id)
     |> change(%{date_out: DateTime.truncate(DateTime.utc_now(), :second), result: result})
     |> Repo.update()
 
     MotivusWbApiWeb.Endpoint.broadcast!(
       "room:client:" <> client_id,
-      "new_msg",
+      "result",
       %{uid: 1, body: body, type: "response", ref: ref, client_id: client_id, task_id: task_id}
     )
+
     current_season = Stats.get_current_season(DateTime.utc_now())
+
     MotivusWbApiWeb.Endpoint.broadcast!(
       "room:worker:" <> id,
       "stats",
